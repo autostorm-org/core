5d7f08d39636ce1ae24615adb204cc132ec6d342
Carlos Balladares <carlosbf@protonmail.com>
2021-04-14 00:58:31 +0000

    Adding theming override for Icon Component
    [#7]
    
    * Add themeing overide

--- packages/common/src/components/Icon/Icon.tsx
+++ packages/common/src/components/Icon/Icon.tsx
@@ -1,20 +1,23 @@
 import React from "react";
+import { WithClassOverride } from "../../types";
 
-type IconProps = {
+type IconProps = WithClassOverride<{
   src: string;
   width: number;
   height: number;
   alt: string;
-};
+}>;
 
-function Icon(props: IconProps) {
+const Icon = (props: IconProps) => {
   return (
     <img
       src={props.src}
       width={props.width}
       height={props.height}
       alt={props.alt}
+      className={`${props.override || ""}`}
     />
   );
-}
+};
 export default Icon;
+export type { IconProps };
