5b9f8a3945ad3500354fda7d68d188fc72369c69
Carlos Balladares <carlosbf@protonmail.com>
2021-05-19 23:22:34 +0000

    Remove local resolution path

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,9 +1,9 @@
 cd ./packages
 cd ./autofica-deprem && yarn build || exit $?
 cd ../
-cd ./autofica-abi && yarn build:gen && yarn test || exit $?
+cd ./autofica-abi && yarn && yarn build:gen && yarn test || exit $?
 cd ../
-cd ./autofica-components && yarn test || exit $?
+cd ./autofica-components && yarn && yarn test || exit $?
 cd ../
-cd ./autofica-multiplier-sdk && yarn test || exit $?
+cd ./autofica-multiplier-sdk  && yarn && yarn test || exit $?
 cd ../
